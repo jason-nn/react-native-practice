41d2077f3298fd13e6abcb6955035c496a277d33
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-13 12:56:26 +0800

    Add text, logo, and button

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+import logo from './assets/logo.png';
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +29,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: { width: 150, height: 150, borderRadius: 6, marginBottom: 20 },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 6,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+  },
 });
+
+export default App;
