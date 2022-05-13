6516d9a0bb0bf67e8cee77d398ff5eb5ee976c6a
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-13 13:18:45 +0800

    Add image picker

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
+import {
+  requestMediaLibraryPermissionsAsync,
+  launchImageLibraryAsync,
+} from 'expo-image-picker';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import logo from './assets/logo.png';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
-      <Text style={styles.instructions}>
-        To share a photo from your phone with a friend, just press the button
-        below!
-      </Text>
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const selectImage = async () => {
+    const permission = await requestMediaLibraryPermissionsAsync;
+
+    if (permission === false) {
+      alert('Permission to access camera roll is required');
+      return;
+    }
+
+    const selectedImage = await launchImageLibraryAsync();
+
+    if (selectedImage.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage(selectedImage.uri);
+    console.log(selectedImage.uri);
+  };
+
+  if (selectedImage === null) {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.instructions}>
+          To share a photo from your phone with a friend, just press the button
+          below!
+        </Text>
+        <TouchableOpacity onPress={selectImage} style={styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage }} style={styles.selectedImage} />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -45,6 +74,11 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
     color: '#fff',
+  },
+  selectedImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 });
 
